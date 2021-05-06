cf50d68cc748bdfccd0658ed60495f2a7704a5a3
unknown <antoniopaulobaciu@gmail.com>
2021-05-06 17:02:50 +0200

    Completed Pokemon moves

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function getID() {
   document.getElementById("run").addEventListener("click", function () {
     pokemonID = document.getElementById("input").value;
     getPokemon();
+    // getEvo();
   });
 }
 getID();
@@ -19,4 +20,26 @@ async function getPokemon() {
   document
     .getElementById("output_img")
     .setAttribute("src", `${sprites.front_default}`);
+
+  // Gets moves & shows different moves for each evo form
+  for (let i = 1; i < 6; i++) {
+    let moves = data.moves[i * 10].move.name;
+    let movesLocation = (document.getElementById(
+      `move_${i}`
+    ).textContent = moves);
+  }
 }
+
+// Gets evolved Pokemon
+
+// async function getEvo() {
+//   let api_url = `https://pokeapi.co/api/v2/evolution-chain/${pokemonID}`;
+//   const response = await fetch(api_url);
+//   const data = await response.json();
+//   const evo = data;
+// }
+
+// EVOLUTION ORDER
+// console.log(evo.chain.species.name); #1
+// console.log(evo.chain.evolves_to[0].species.name); #2
+// console.log(evo.chain.evolves_to[0].evolves_to[0].species.name); #3
