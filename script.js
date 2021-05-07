f41ead8cdcd9965b8a4c27ee890de8ec4f03bb72
unknown <antoniopaulobaciu@gmail.com>
2021-05-07 16:42:53 +0200

    Applied styling, added images, made the first letter capitalized

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ function getID() {
 }
 getID();
 
+// Capitalize first letter
+function capitalizeLTR(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 // Gets data from API
 async function getPokemon() {
   let api_url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`;
   const response = await fetch(api_url);
   const data = await response.json();
   const { name, id, sprites } = data;
-  document.getElementById("output_name").textContent = `Name: ${name}`;
+  document.getElementById("output_name").textContent = `Name: ${capitalizeLTR(name)}`;
   document.getElementById("output_id").textContent = `ID: #${id}`;
   document
     .getElementById("output_img")
@@ -23,23 +28,9 @@ async function getPokemon() {
 
   // Gets moves & shows different moves for each evo form
   for (let i = 1; i < 6; i++) {
-    let moves = data.moves[i * 10].move.name;
-    let movesLocation = (document.getElementById(
-      `move_${i}`
-    ).textContent = moves);
+    let moves = data.moves[i * 6].move.name;
+    let movesLocation = document.getElementById(`move_${i}`);
+    movesLocation.textContent = moves;
+    movesLocation.style.display = "block";
   }
 }
-
-// Gets evolved Pokemon
-
-// async function getEvo() {
-//   let api_url = `https://pokeapi.co/api/v2/evolution-chain/${pokemonID}`;
-//   const response = await fetch(api_url);
-//   const data = await response.json();
-//   const evo = data;
-// }
-
-// EVOLUTION ORDER
-// console.log(evo.chain.species.name); #1
-// console.log(evo.chain.evolves_to[0].species.name); #2
-// console.log(evo.chain.evolves_to[0].evolves_to[0].species.name); #3
